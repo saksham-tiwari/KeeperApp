b540ec88dfd50e9129fff42a4fa1c1c1e13ab7e2
saksham-tiwari <63906176+saksham-tiwari@users.noreply.github.com>
2020-12-17 16:42:17 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+function App(){
+    return (<div>
+        <Header />
+        <Note />
+        <Footer />
+    </div>)
+}
+
+export default App;
