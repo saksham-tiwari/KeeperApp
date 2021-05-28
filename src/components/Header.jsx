9bc25072038e9f1170e9873fc1777cd111d86878
saksham-tiwari <63906176+saksham-tiwari@users.noreply.github.com>
2021-05-28 21:46:07 +0530

    Add files via upload

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import WbIncandescentIcon from "@material-ui/icons/WbIncandescent";
 
-function Header(){
-    return (<header><h1>Keeper</h1></header>)
+function Header() {
+  return (
+    <header>
+      <h1>
+        <WbIncandescentIcon />
+        Keeper
+      </h1>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-function Note(){
-    return (<div className="note">
-        <h1>This is the note title</h1>
-        <p>This is the note content.</p>
-    </div>)
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}>
+        <DeleteIcon />
+      </button>
+    </div>
+  );
 }
 
 export default Note;
